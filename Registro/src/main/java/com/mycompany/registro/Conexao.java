4d16e912a351934bbcdaa36b8f44ef09afb82c7a
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-02 21:23:42 -0300

    by Micael

---
+++ Registro/src/main/java/com/mycompany/registro/Conexao.java
@@ -0,0 +1,16 @@
+package com.mycompany.registro;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://postgres:2L6OyaLq3lQLsOE8s8YQ@containers-us-west-59.railway.app:5793/railway";
+        String username = "postgres";
+        String password = "2L6OyaLq3lQLsOE8s8YQ";
+
+        Connection conexao = DriverManager.getConnection(url, username, password);
+        return conexao;
+    }
+}
